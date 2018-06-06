449a70211429942baea480c2bdcf22203a713cd0
dkfl1995 <vlaio.95@gmail.com>
2018-06-06 21:56:57 +0300

    First controller

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -1,19 +1,31 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbsractController;
 
-class WelcomeController extends Controller
+class WelcomeController extends AbstractController
 {
     /**
-     * @Route("/welcome", name="welcome")
+     * @Route("/welcome"/{name}, name="welcome")
+     * @param string &name
+     * @return Response
      */
-    public function index()
+    public function index(string $name)
     {
-        return $this->render('welcome/index.html.twig', [
-            'controller_name' => 'WelcomeController',
-        ]);
-    }
+        return $this->render('welcome/index.html.twig';
+        }
+}
+
+/**
+ * ("/", name="hello_page")
+
+ */
+public function homepage()
+ {$name = $request->query->get(key:"name");
+    return $this->render('hello_page.html.twig',[
+        'some_var'=>$name,
+         'some_var01'=>'World01',
+]);
 }
